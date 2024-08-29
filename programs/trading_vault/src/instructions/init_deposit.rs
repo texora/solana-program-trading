775a2a1cc462eecbc09ce8776369269c2c362fe1
texora <ultantaylor@gmail.com>
2024-08-29 22:40:29 +0200

    Update init_deposit.rs

--- programs/trading_vault/src/instructions/init_deposit.rs
+++ programs/trading_vault/src/instructions/init_deposit.rs
@@ -8,60 +8,30 @@ use crate::{constants::TOKEN_DECIMALS, error::*, User, Vault};
 pub struct InitDeposit<'info> {
     #[account(mut)]
     pub leader: Signer<'info>,
-    #[account(
-        init_if_needed,
-        seeds = [b"user", leader.key().as_ref()],
-        bump,
-        payer = leader,
-        space = User::LEN
-    )]
+    #[account(mut)]
     pub user: Account<'info, User>,
 
     #[account(mut)]
     pub backend_wallet: Signer<'info>,
 
-    #[account(
-        seeds = [b"vault", leader.key().as_ref()],
-        bump,
-    )]
+    #[account(mut)]
     pub vault: Account<'info, Vault>,
     /// CHECK:
-    #[account(
-        seeds = [b"vault_authority"],
-        bump,
-        )]
+    #[account(mut)]
     pub vault_authority: AccountInfo<'info>,
 
     // Create mint account
     // Same PDA as address of the account and mint/freeze authority
-    #[account(
-        seeds = [b"mint"],
-        bump,
-        mint::decimals = TOKEN_DECIMALS,
-        mint::authority = mint_account.key(),
-        mint::freeze_authority = mint_account.key(),
-
-    )]
+    #[account(mut)]
     pub mint_account: Account<'info, Mint>,
 
-    #[account(
-        init_if_needed,
-        payer = backend_wallet,
-        constraint = vault_pay_token_account.mint == leader_pay_token_account.mint,
-        constraint = vault_pay_token_account.owner == vault_authority.key(),
-        space = 165,
-    )]
+    #[account(mut)]
     pub vault_pay_token_account: Account<'info, TokenAccount>,
     #[account(mut)]
     pub leader_pay_token_account: Account<'info, TokenAccount>,
     // Create Associated Token Account, if needed
     // This is the account that will hold the minted tokens
-    #[account(
-        init_if_needed,
-        payer = leader,
-        associated_token::mint = mint_account,
-        associated_token::authority = leader,
-    )]
+    #[account(mut)]
     pub leader_token_account: Account<'info, TokenAccount>,
 
     pub system_program: Program<'info, System>,
@@ -104,24 +74,6 @@ pub fn init_deposit(
 
     let bond_amount =
         params.initial_deposit / 1_000_000 * 10u64.pow(ctx.accounts.mint_account.decimals as u32);
-
-    // PDA signer seeds
-    let signer_seeds: &[&[&[u8]]] = &[&[b"mint", &[ctx.bumps.mint_account]]];
-
-    // Invoke the mint_to instruction on the token program
-    mint_to(
-        CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            MintTo {
-                mint: ctx.accounts.mint_account.to_account_info(),
-                to: ctx.accounts.leader_token_account.to_account_info(),
-                authority: ctx.accounts.mint_account.to_account_info(), // PDA mint authority, required as signer
-            },
-        )
-        .with_signer(signer_seeds), // using PDA to sign
-        bond_amount, // Mint tokens
-    )?;
-    msg!("Token minted successfully.");
 
     user.deposit_value = params.initial_deposit;
     user.bond_amount = bond_amount;
