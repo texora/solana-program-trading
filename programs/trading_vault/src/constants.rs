a423f22543aa2096545236b729c3d84b0f8cef93
texora <ultantaylor@gmail.com>
2024-08-29 22:40:48 +0200

    Update constants.rs

--- programs/trading_vault/src/constants.rs
+++ programs/trading_vault/src/constants.rs
@@ -1,8 +1,4 @@
 use anchor_lang::prelude::*;
 
 #[constant]
-pub const  VAULT: &str = "vault";
-pub const VAULT_AUTHORITY: &str = "vault_authority";
-pub const  USER_ACCOUNT: &str = "user_account";
-
 pub const TOKEN_DECIMALS: u8 = 6;
